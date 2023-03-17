f7b2c1b4f70a6cb48b170f953786e41b97f37205
romeomaria.magtanong <romeomaria.magtanong@gcash.com>
2023-03-17 15:58:47 +0800

    Add join group tests

--- app/group/tests/test_group_api.py
+++ app/group/tests/test_group_api.py
@@ -9,8 +9,13 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Group
+from group.serializers import GroupSerializer
 
 GROUP_URL = reverse('group:group-list')
+
+
+def detail_url(group_id):
+    return reverse('group:group-join', args=[group_id])
 
 
 def create_user(**params):
@@ -22,6 +27,14 @@ def create_user_payload():
         'email': 'test@example.com',
         'password': 'test1234',
         'name': 'Test'
+    }
+
+
+def create_other_user_payload():
+    return {
+        'email': 'other@example.com',
+        'password': 'other1234',
+        'name': 'Other'
     }
 
 
@@ -54,3 +67,45 @@ class PrivateGroupExpenseApiTests(TestCase):
         self.assertEqual(res.data['group_name'], group.group_name)
         self.assertEqual(self.user, group.group_members.first())
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_list_groups_member_only(self):
+        self.client.post(GROUP_URL, {'group_name': 'Group 1'})
+        self.client.post(GROUP_URL, {'group_name': 'Group 2'})
+
+        self.user = create_user(**create_other_user_payload())
+        self.client.force_authenticate(self.user)
+
+        res = self.client.get(GROUP_URL)
+
+        self.assertEqual(len(res.data), 0)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_join_group(self):
+        group = self.client.post(GROUP_URL, {'group_name': 'Test Group'})
+        group_id = group.data['id']
+
+        self.user = create_user(**create_other_user_payload())
+        self.client.force_authenticate(self.user)
+
+        res = self.client.post(detail_url(group_id), payload={'group_name': 'Test Group'})
+
+        group = Group.objects.get(id=group_id)
+        serializer = GroupSerializer(group)
+
+        self.assertEqual(len(serializer.data['group_members']), 2)
+        self.assertEqual(serializer.data['group_members'][0]['name'], 'Test')
+        self.assertEqual(serializer.data['group_members'][1]['name'], 'Other')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_join_group_existing_member(self):
+        group = self.client.post(GROUP_URL, {'group_name': 'Test Group'})
+        group_id = group.data['id']
+
+        res = self.client.post(detail_url(group_id), payload={'group_name': 'Test Group'})
+
+        group = Group.objects.get(id=group_id)
+        serializer = GroupSerializer(group)
+
+        self.assertEqual(len(serializer.data['group_members']), 1)
+        self.assertEqual(serializer.data['group_members'][0]['name'], 'Test')
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/group/views.py
+++ app/group/views.py
@@ -2,8 +2,11 @@
 Views for Group API
 """
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Group
 from group import serializers
@@ -37,3 +40,18 @@ class GroupViewSet(viewsets.ModelViewSet):
         """
         user = self.request.user
         return user.groups.all()
+
+    @action(detail=True, methods=['post'])
+    def join(self, request, *args, **kwargs):
+        user = request.user
+        group_id = self.kwargs['pk']
+
+        group = Group.objects.get(id=group_id)
+
+        if group.group_members.filter(id=user.id).exists():
+            return Response({'detail': 'User is already a member of this group.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        group.group_members.add(user)
+        user.groups.add(group)
+
+        return Response({'detail': 'User has been added to the group.'}, status=status.HTTP_200_OK)
